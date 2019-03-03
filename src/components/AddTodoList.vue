6cc79aa65496588bbe7166d43a3ed355d63ad50a
wanmaoor <wanmao1284879375@outlook.com>
2019-03-03 20:51:02 +0800

    modify style

--- src/components/AddTodoList.vue
+++ src/components/AddTodoList.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="container-fluid">
+  <div class="container">
     <form @submit="addItem">
-      <div class="form-row">
-        <div class="col-md-10 my-2">
+      <div class="form-row form">
+        <div class="col-sm-9 my-2 ">
           <label class="sr-only" for="inlineFormInputName">Name</label>
           <input type="text" v-model="item" class="form-control" id="inlineFormInputName" placeholder="添加事件清单">
         </div>
-        <div class="col-md-2 my-2">
+        <div class="col-sm-1 my-2">
           <button type="submit" class="btn btn-primary">提交</button>
         </div>
       </div>
     </form>
   </div>
 </template>
+
+<style>
+
+  .form{
+    display: flex;
+    justify-content: center;
+    margin: auto;
+  }
+</style>
+
 
 <script>
 import uuid from 'uuid';
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div class="alert alert-primary show" role="alert" v-for="todo in todos" :key="todo.id">
+    <div class="alert alert-primary show col-sm-10" role="alert" v-for="todo in todos" :key="todo.id">
       <span>{{todo.item}}</span>
-      <button type="button" class="btn btn-danger" @click="$emit('remove-item', todo.id)">删除</button>
+      <button type="button" class="btn btn-danger col-sm-2" @click="$emit('remove-item', todo.id)">删除</button>
     </div>
   </div>
 </template>
@@ -15,6 +15,7 @@ export default {
 <style scoped>
   .show{
     display: flex;
+    margin: .7rem auto;
   }
   .show span{
     flex: 10;
@@ -22,6 +23,11 @@ export default {
   }
   .show button{
     flex: 1
+  }
+  .show span{
+    vertical-align: center;
+    font-size: 1.2rem;
+    margin: auto;
   }
 </style>
 
